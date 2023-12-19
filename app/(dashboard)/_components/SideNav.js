bd084a00cbbadca1d1629d92147fa66c626ce664
Manish Chand <manishchandr349@gmail.com>
2023-12-19 20:18:07 +0530

    Adding

--- app/(dashboard)/_components/SideNav.js
+++ app/(dashboard)/_components/SideNav.js
@@ -31,11 +31,11 @@ function SideNav() {
      <div className='p-5 border-b'>
          <Image src='/logo.svg' width={150} height={100}/>
      </div>
-     <div className='flex flex-col float-left'>
+     <div className='flex flex-col float-left w-full'>
 
      {
           menuList.map((item, index)=>(
-               <button className={`flex gap-2 p-4 px-6 hover:bg-gray-100 w-full
+               <button className={`flex gap-2 p-4  px-6 hover:bg-gray-100 w-full
                 text-gray-500 ${activeIndex == index?'bg-blue-50 text-primary':null} `}
                 onClick={()=> setActiveIndex(index)}
                 >
